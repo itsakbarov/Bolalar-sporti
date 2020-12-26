e3de01d91bf2add8a9ddf8013433f602ab5de1bf
Sardor <asrorjon5538@gmail.com>
2020-12-26 07:55:50 +0500

    bugs fixed.

--- assets/script/main.js
+++ assets/script/main.js
@@ -79,9 +79,6 @@ $('.responsive').slick({
                 slidesToScroll: 1
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
     ]
 });
 
@@ -104,12 +101,3 @@ var swiper = new Swiper('.swiper-container', {
         clickable: true,
     },
 });
-
-
-
-
-
-
-
-
-
